d3bc4a9f390d34e8a728ee3b7dfb2926d0d50664
Abdulloh <tohirovsarvar219@gmail.com>
2023-01-01 12:00:00 +0500

    commit 2023-01-01 12:00:00

--- script.py
+++ script.py
@@ -7,12 +7,12 @@ end_date = datetime(2023, 12, 31)
 current_date = start_date
 
 while current_date <= end_date:
-    date_str = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    date_str = current_date.strftime("%Y-%m-%d 12:00:00")
     
     with open("file.txt", "a") as f:
-        f.write(f"data {date_str}\n")
+        f.write(date_str + "\n")
     
     os.system("git add .")
-    os.system(f'git commit --date="{date_str}" -m "commit on {date_str}"')
+    os.system(f'git commit --date="{date_str}" -m "commit {date_str}"')
     
-    current_date += timedelta(days=1)       
+    current_date += timedelta(days=1)
